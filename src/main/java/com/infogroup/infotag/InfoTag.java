6c6a565fb83947118626c9614443ee76e66b70a3
pixar02 <pixar.pf@gmail.com>
2018-05-14 21:59:55 +0200

    config

--- src/main/java/com/infogroup/infotag/InfoTag.java
+++ src/main/java/com/infogroup/infotag/InfoTag.java
@@ -9,18 +9,23 @@ public class InfoTag extends JavaPlugin {
 
     Logger logger;
     Metrics metrics;
+
     public void onEnable() {
         logger = this.getLogger();
         metrics = new Metrics(this);
 
-        
+
         logger.info("InfoTag has been enabled!");
     }
 
 
     public void onDisable() {
 
-
         logger.info("InfoTag has been disabled!");
+    }
+
+    //Check if dependencies are installed
+    private void dependencies(){
+
     }
 }
